fa6a45a839fb9e47b7aaf6a566555c0b41623e9a
Junjie Yang <junjie.yang@jhu.edu>
2021-02-16 14:10:52 -0500

    ORM finished

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,27 +1,26 @@
+import org.sql2o.Connection;
+import org.sql2o.Sql2o;
+import org.sql2o.Sql2oException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.sql.*;
 
 public class Demo {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         try (Connection conn = getConnection()) {
             String sql = "CREATE TABLE IF NOT EXISTS courses("
                     + "offeringName VARCHAR(15) NOT NULL PRIMARY KEY,"
                     + "title VARCHAR(50) NOT NULL"
                     + ");";
-            Statement st = conn.createStatement();
-            st.execute(sql);
+            conn.createQuery(sql).executeUpdate();
+            Course course = new Course("EN.601.280", "Full-Stack JavaScript");
+            add(conn, course);
 
-            sql = "INSERT INTO courses (offeringName, title)"
-                    + "VALUES ('EN.601.226', 'Data Structures');";
-            st.execute(sql);
-
-        } catch (URISyntaxException | SQLException e) {
+        } catch (URISyntaxException | Sql2oException e) {
             e.printStackTrace();
         }
     }
 
-    private static Connection getConnection() throws URISyntaxException, SQLException {
+    private static Connection getConnection() throws URISyntaxException,Sql2oException {
         String databaseUrl = System.getenv("DATABASE_URL");
         if (databaseUrl == null) {
             throw new URISyntaxException(databaseUrl, "DATABASE_URL is not set");
@@ -34,6 +33,14 @@ public class Demo {
         String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
                 + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
-        return DriverManager.getConnection(dbUrl, username, password);
+        Sql2o sql2o = new Sql2o(dbUrl, username, password);
+        return sql2o.open();
+    }
+
+    private static void add(Connection conn, Course course) throws Sql2oException {
+        String sql = "INSERT INTO courses(offeringName, title) VALUES(:offeringName, :title);";
+        conn.createQuery(sql)
+                .bind(course)
+                .executeUpdate();
     }
 }
