3f139c70f41721ec4a31cb4b184ac067bd56448b
Junjie Yang <junjie.yang@jhu.edu>
2021-02-19 01:08:31 -0500

    Testing API finished

--- src/main/java/ApiServer.java
+++ src/main/java/ApiServer.java
@@ -1,24 +1,31 @@
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import org.sql2o.Sql2o;
+import spark.Spark;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static spark.Spark.*;
 
 public class ApiServer {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws URISyntaxException {
         port(getHerokuAssignedPort());
-        List<Course> courses = new ArrayList<>();
-        courses.add(new Course("EN.500.112","GATEWAY COMPUTING: JAVA"));
-        courses.add(new Course("EN.601.220","INTERMEDIATE PROGRAMMING"));
-        courses.add(new Course("EN.601.226","DATA STRUCTURES"));
-        courses.add(new Course("EN.601.229","COMPUTER SYSTEM FUNDAMENTALS"));
-        courses.add(new Course("EN.601.231","AUTOMATA and COMPUTATION THEORY"));
 
-        Gson gson = new Gson();
-        get("/api/courses",  (req, res) -> {
-            res.type("application/json");
-            return courses;
-        }, gson::toJson);
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        CourseDao courseDao = getCourseDao();
+
+        get("/api/courses", (req, res) -> {
+            try {
+                List<Course> courses = courseDao.readAll();
+                res.type("application/json");
+                return gson.toJson(courses);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
     }
 
     private static int getHerokuAssignedPort() {
@@ -29,6 +36,26 @@ public class ApiServer {
         }
         //return default port if heroku-port isn't set (i.e. on localhost)
         return 4567;
+    }
+
+    private static CourseDao getCourseDao() throws URISyntaxException {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':'
+                + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+        Sql2o sql2o = new Sql2o(dbUrl, username, password);
+        return new Sql2oCourseDao(sql2o);
+    }
+
+    /**
+     * Stop the server.
+     */
+    public static void stop() {
+        Spark.stop();
     }
 }
 
---
+++ src/test/java/ApiServerTest.java
@@ -0,0 +1,38 @@
+import com.google.gson.Gson;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.UnirestException;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import spark.Spark;
+
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+class ApiServerTest {
+
+    private final static String BASE_URL = "http://localhost:4567";
+    private static final Gson gson = new Gson();
+
+    @BeforeAll
+    static void runApiServer() throws URISyntaxException {
+        ApiServer.main(null); // run the server
+    }
+
+    @AfterAll
+    static void stopApiServer() {
+        ApiServer.stop();
+    }
+
+    @Test
+    public void getCoursesWorks() throws UnirestException {
+        final String URL = BASE_URL + "/api/courses";
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(URL).asJson();
+        assertEquals(200, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+}
