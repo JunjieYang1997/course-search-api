6a1bf439a95fcad008d30741d87381b53fde6b6e
Junjie Yang <junjie.yang@jhu.edu>
2021-02-18 21:23:12 -0500

    PUT Course finished

--- src/main/java/ApiServer.java
+++ src/main/java/ApiServer.java
@@ -1,5 +1,6 @@
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonSyntaxException;
 import org.sql2o.Sql2o;
 import spark.Spark;
 
@@ -7,6 +8,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import static spark.Spark.*;
 
@@ -17,15 +19,86 @@ public class ApiServer {
         Gson gson = new GsonBuilder().disableHtmlEscaping().create();
         CourseDao courseDao = getCourseDao();
 
+        exception(ApiError.class, (ex, req, res) -> {
+            // Handle the exception here
+            Map<String, String> map = Map.of("status", ex.getStatus() + "",
+                    "error", ex.getMessage());
+            res.body(gson.toJson(map));
+            res.status(ex.getStatus());
+        });
+
+
         get("/api/courses", (req, res) -> {
             try {
-                List<Course> courses = courseDao.readAll();
-                res.type("application/json");
+                String title = req.queryParams("title");
+                List<Course> courses;
+                if (title != null) {
+                    courses = courseDao.readAll(title);
+                } else {
+                    courses = courseDao.readAll();
+                }
                 return gson.toJson(courses);
             } catch (DaoException ex) {
                 throw new ApiError(ex.getMessage(), 500);
             }
         });
+
+        get("/api/courses/:offeringName", (req, res) -> {
+            try {
+                String offeringName = req.params("offeringName");
+                Course course = courseDao.read(offeringName);
+                if (course == null) {
+                    throw new ApiError("Resource not found", 404); // Bad request
+                }
+                return gson.toJson(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+
+        post("/api/courses", (req, res) -> {
+            try {
+                Course course = gson.fromJson(req.body(), Course.class);
+                courseDao.create(course.getOfferingName(), course.getTitle());
+                res.status(201);
+                return gson.toJson(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+
+        delete("/api/courses/:offeringName", (req, res) -> {
+            try {
+                String offeringName = req.params("offeringName");
+                Course course = courseDao.delete(offeringName);
+                if (course == null) {
+                    throw new ApiError("Resource doesn't exist", 404);
+                }
+                return gson.toJson(course);
+            } catch (DaoException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+
+        put("/api/courses/:offeringName", (req, res) -> {
+            try {
+                String offeringName = req.params("offeringName");
+                Course course = gson.fromJson(req.body(), Course.class);
+                if (!course.getOfferingName().equals(offeringName)) {
+                    throw new ApiError("offering name does not match the resource identifier", 400);
+                }
+                course = courseDao.update(course.getOfferingName(), course.getTitle());
+                if (course == null) {
+                    throw new ApiError("Resource not found", 404);
+                }
+                return gson.toJson(course);
+            } catch (DaoException | JsonSyntaxException ex) {
+                throw new ApiError(ex.getMessage(), 500);
+            }
+        });
+
+
+        after((req, res) -> res.type("application/json"));
     }
 
     private static int getHerokuAssignedPort() {
--- src/test/java/ApiServerTest.java
+++ src/test/java/ApiServerTest.java
@@ -9,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import spark.Spark;
 
 import java.net.URISyntaxException;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -34,5 +35,107 @@ class ApiServerTest {
         HttpResponse<JsonNode> jsonResponse = Unirest.get(URL).asJson();
         assertEquals(200, jsonResponse.getStatus());
         assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void getCoursesGivenOfferingName() throws UnirestException {
+        final String OFFERING_NAME = "EN.601.226";
+        final String URL = BASE_URL + "/api/courses/" + OFFERING_NAME;
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(URL).asJson();
+        assertEquals(200, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void getCoursesGivenOfferingNameNotInDatabase() throws UnirestException {
+        final String OFFERING_NAME = "EN.000.999";
+        final String URL = BASE_URL + "/api/courses/" + OFFERING_NAME;
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(URL).asJson();
+        assertEquals(404, jsonResponse.getStatus());
+    }
+
+    @Test
+    public void getCoursesGivenTitle() throws UnirestException {
+        final String TITLE = "data";
+        final String URL = BASE_URL + "/api/courses?title=" + TITLE;
+        HttpResponse<JsonNode> jsonResponse = Unirest.get(URL).asJson();
+        assertEquals(200, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void postCourseWorks() throws UnirestException {
+        // This test will break if "EN.601.421" is already in database
+        Course course = new Course("EN.601.421", "Object-Oriented Software Engineering");
+        final String URL = BASE_URL + "/api/courses";
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                .body(gson.toJson(course)).asJson();
+        assertEquals(201, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void postCourseWithIncompleteData() throws UnirestException {
+        Map<String, String> course = Map.of("title", "Made-up Course");
+        final String URL = BASE_URL + "/api/courses";
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                .body(gson.toJson(course)).asJson();
+        assertEquals(500, jsonResponse.getStatus());
+    }
+
+    @Test
+    public void postCourseThatAlreadyExist() throws UnirestException {
+        // This test will break if "EN.601.226" is not in the database
+        Course course = new Course("EN.601.226", "Data Structures");
+        final String URL = BASE_URL + "/api/courses";
+        HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                .body(gson.toJson(course)).asJson();
+        assertEquals(500, jsonResponse.getStatus());
+    }
+
+    @Test
+    public void deleteCourseWorks() throws UnirestException {
+        // This test will break if "EN.601.226" does not exists in database
+        final String OFFERING_NAME = "EN.601.226";
+        final String URL = BASE_URL + "/api/courses/" + OFFERING_NAME;
+        HttpResponse<JsonNode> jsonResponse = Unirest.delete(URL).asJson();
+        assertEquals(200, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void deleteCourseNotInDatabase() throws UnirestException {
+        // This test will break if "EN.000.999" exists in database
+        final String OFFERING_NAME = "EN.000.999";
+        final String URL = BASE_URL + "/api/courses/" + OFFERING_NAME;
+        HttpResponse<JsonNode> jsonResponse = Unirest.delete(URL).asJson();
+        assertEquals(404, jsonResponse.getStatus());
+    }
+
+    @Test
+    public void updateCourseWorks() throws UnirestException {
+        // This test will break if "EN.601.621" does not exist in database
+        Course course = new Course("EN.601.621", "OOSE");
+        final String URL = BASE_URL + "/api/courses/" + course.getOfferingName();
+        HttpResponse<JsonNode> jsonResponse = Unirest.put(URL).body(gson.toJson(course)).asJson();
+        assertEquals(200, jsonResponse.getStatus());
+        assertNotEquals(0, jsonResponse.getBody().getArray().length());
+    }
+
+    @Test
+    public void updateCourseNotInDatabase() throws UnirestException {
+        // This test will break if "EN.000.999" exists in database
+        Course course = new Course("EN.000.999", "OOSE");
+        final String URL = BASE_URL + "/api/courses/" + course.getOfferingName();
+        HttpResponse<JsonNode> jsonResponse = Unirest.put(URL).body(gson.toJson(course)).asJson();
+        assertEquals(404, jsonResponse.getStatus());
+    }
+
+    @Test
+    public void updateCourseIncorrectOfferingName() throws UnirestException {
+        Course course = new Course("EN.601.621", "OOSE");
+        final String URL = BASE_URL + "/api/courses/" + "EN.601.421";
+        HttpResponse<JsonNode> jsonResponse = Unirest.put(URL).body(gson.toJson(course)).asJson();
+        assertEquals(400, jsonResponse.getStatus());
     }
 }
